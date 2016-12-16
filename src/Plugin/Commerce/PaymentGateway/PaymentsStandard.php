769e67bcd22750dd64aa2acd94c30a10106d242a
Matt Glaman <nmd.matt@gmail.com>
2016-12-16 10:00:32 -0600

    Save payment on return

--- src/Plugin/Commerce/PaymentGateway/PaymentsStandard.php
+++ src/Plugin/Commerce/PaymentGateway/PaymentsStandard.php
@@ -31,8 +31,39 @@ class PaymentsStandard extends OffsitePaymentGatewayBase implements PaymentsStan
    * {@inheritdoc}
    */
   public function onRedirectReturn(OrderInterface $order) {
-    // Create and save payment method.
-    $stop = null;
+    $current_request = \Drupal::getContainer()->get('request_stack')->getCurrentRequest();
+
+    // mc_gross = 89.50
+    // invoice = 3-TIMESTAMP
+    // protection_eligibility
+    // item_number1
+    // payer_id
+    // tax
+    // payment_date
+    // payment_status (Pending)
+    // notify_version
+    // payer_status
+    // payer_email
+    // verify_sign
+    // tnx_id
+    // pending_reason
+    // payment_gross
+    // auth
+
+    // Create the payment.
+    $payment_storage = \Drupal::entityTypeManager()->getStorage('commerce_payment');
+    $payment = $payment_storage->create([
+      'state' => 'authorization',
+      'amount' => $order->getTotalPrice(),
+      // Gateway plugins cannot reach their matching config entity directly.
+      'payment_gateway' => $order->payment_gateway->entity->id(),
+      'order_id' => $order->id(),
+      'test' => $this->getMode() == 'test',
+      'remote_id' => $current_request->request->get('txn_id'),
+      'remote_state' => $current_request->request->get('payment_status'),
+      'authorized' => REQUEST_TIME,
+    ]);
+    $payment->save();
   }
 
   /**
